2c805b7a81f0fa8a6c49688074a666296f6ec2d9
dileeprdm <120439697+dileeprdm@users.noreply.github.com>
2023-09-07 11:26:05 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -5,8 +5,8 @@ terraform {
     key = "cicd/dev_infra/terraform.tfstate"
     region = "us-east-1"
     profile = "IOT"
-    access_key = AKIA2F22NE445HI3OGCT
-    secret_key = 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
+    access_key = "AKIA2F22NE445HI3OGCT"
+    secret_key = "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"
   }
 }
 
