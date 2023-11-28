1ebf9ac200f852e490d5005d4e52b1706e26497b
Terraform_AFT <120720602+Umagayathri1405@users.noreply.github.com>
2023-11-28 16:29:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,6 @@
 provider "aws" {
   region = var.AWS_Region
   profile = var.Terraform_Profile
-  shared_credentials_files = "~/.aws/credentials"
-  #profile = "IOT"
+  #shared_credentials_files = "~/.aws/credentials"
 }
 
